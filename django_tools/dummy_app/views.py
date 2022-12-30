69df900f05da1b58e97ce8e08fb2a132b375413f
JudahSan <baraka.judah@yahoo.com>
2022-12-30 21:38:20 +0300

    project view and template

--- django_tools/dummy_app/views.py
+++ django_tools/dummy_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Anime
 # Create your views here.
+
+def anime_list(request):
+  anime_objects = Anime.objects.all()
+  return render(request,'dummy_app/anime_list.html',{'anime_objects':anime_objects})
+  
